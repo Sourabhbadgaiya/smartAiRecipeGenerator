8e816bbca127b7d205815ccdbb35059af7796eb1
Sourabhbadgaiya <sourabhbadgaiya273@gmail.com>
2025-04-02 12:21:04 +0530

    Update redis.config.js

--- server/src/config/redis.config.js
+++ server/src/config/redis.config.js
@@ -1,9 +1,9 @@
 import Redis from "ioredis";
 
 const redisClient = new Redis({
-  port: 6379,
-  host: "127.0.0.1",
-  password: "",
+  port: 11953,
+  host: "redis-11953.c305.ap-south-1-1.ec2.redns.redis-cloud.com",
+  password: "28Bm1U7PqtTm9Lbme4ZGcJ3LMkuAg8E5",
   retryStrategy: (times) => Math.min(times * 50, 2000), // Retry with max 2s delay
 });
 
